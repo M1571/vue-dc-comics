e5e35e59d06dd4edb140813ddf4f4ab4a5402e7f
M1571 <94639639+M1571@users.noreply.github.com>
2022-03-10 01:58:57 +0100

    footer set

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -8,19 +8,47 @@
       <!-- ------------------------------- -->
 
       <section class="footer-main">
-          <ul>
-              <li>DC COMICS</li>
-              <li>DC</li>
-              <li>SITES</li>
-          </ul>
+
+          <div class="container">
+
+              <div class="col">
+                  <div class="col-list">
+
+                        <h4>DC Comics</h4>
+                        <ul>
+                            <li>link 1</li>
+                            <li>link 2</li>
+                            <li>link 3</li>
+                        </ul>
+
+                  </div>
+              </div>
+
+              <div class="col">
+                  <div class="col-list">
+
+                        <h4 class="list_title">DC</h4>
+                        <ul class="list_menu">
+                            <li>link 1</li>
+                            <li>link 2</li>
+                            <li>link 3</li>
+                        </ul>
+
+                  </div>
+              </div>
+
+          </div>
+          
       </section>
 
       <!-- ------------------------------- -->
 
       <section class="footer-bottom">
+
           <button>
               SIGN-UP NOW!
           </button>
+
           <div>
               <span>FOLLOW US</span>
               <ul>
@@ -31,6 +59,7 @@
                   <li>Maps</li>
               </ul>
           </div>
+
       </section>
 
       <!-- ------------------------------- -->
@@ -55,12 +84,23 @@ export default {
 
 <style lang="scss" scoped>
 
+@import '../assets/scss/mixins.scss';
+
 .footer {
 
     color: white;
 
     .footer-main {
         background: url('../assets/img/footer-bg.jpg');
+        padding: 40px 0;
+
+        .container {
+            display: flex;
+
+            .col {
+                width: calc(100% / 9);
+            }
+        }
     }
 
     // -------------------------------
